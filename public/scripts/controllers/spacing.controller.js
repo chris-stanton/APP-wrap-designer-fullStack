ca80e0671a02989bd4bb8d2ef8a993649bf2dfa9
Chris Stanton <cstanton0760@yahoo.com>
2017-03-19 16:16:45 -0500

    created spacing view and data path from view to controller controller to factory

--- public/scripts/controllers/spacing.controller.js
+++ public/scripts/controllers/spacing.controller.js
@@ -5,28 +5,10 @@ console.log('SpacingController running');
 var self = this;
 self.testMessage = 'SC WORKING STATUS';
 
-//   self.newArray = [];
-//
-//   getFunction();
-//
-//   function getFunction() {
-//     $http.get('</sample-url>')
-//       .then(function(response) {
-//         console.log(response);
-//         self.newArray = response.data;
-//       })//end of .then
-//   }//end of getHeroes()
-//
-//
-// //deletes by ID
-//   self.deleteFunction = function('<sample-perameter-object>;') {
-//     $http.delete('</sample-url>' + object.id)
-//       .then(function(response) {
-//         console.log('deleteHero()');
-//         getHeroes();
-//       })//end of .then
-//   }//end of self.deleteHero()
-//   }]);//end of controller
+self.blankData = FactoryFactory.blankFactory;
+
+
+
 
 
 }]); // end controller code block
--- public/scripts/factories/factory.factory.js
+++ public/scripts/factories/factory.factory.js
@@ -1,11 +1,22 @@
 myApp.factory('FactoryFactory',['$http',function($http) {
 console.log('FactoryFactory running');
 
+var blankFactory = { list: [] };
 
+getBlanks();
+
+//gets blanks for drop down options
+function getBlanks() {
+    $http.get('/spacing')
+      .then(function(response) {
+        console.log(response);
+        self.blankFactory.list = response.data;
+    })//end of .then
+}//end of blanks()
 
 
 
   return {
-
+    blankFactory : blankFactory
   }
 }]);
